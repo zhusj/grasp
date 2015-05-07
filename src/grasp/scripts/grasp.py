648a87c2b96cce9c186f68b79b9ccf365cf3a1a8
zhusj <vincentzhu122@gmail.com>
2015-05-06 20:59:50 -0400

    save as yaml

--- src/grasp/scripts/grasp.py
+++ src/grasp/scripts/grasp.py
@@ -4,6 +4,8 @@ import sys
 import tf
 import numpy
 import math
+import yaml
+
 
 
 import std_msgs.msg
@@ -28,13 +30,13 @@ def grasp_main():
 
   object_name = 'expo_dry_erase_board_eraser'
   # print object_name
-  grasp = {}
+  # grasp = {}
   index = 0
   # grasp = [[0 for x in range(5)] for x in range(5)] 
   # print grasp
 
   # if object_name == 'oreo_mega_stuf':
-  grasp[object_name] = {}
+  grasp = []
   # pose = Pose()
   # pose.position.x = 0
   # pose.position.y = 0
@@ -50,48 +52,57 @@ def grasp_main():
   alpha_range = 30
   beta_range = 0
   gamma_range = 0
-  for i in range(0, beta_range/angle_resolution + 1):
-    pose = Pose()
-    pose.position.x = 0
-    pose.position.y = 0
-    pose.position.z = 0
-    beta = d_to_r * (- beta_range/2 + i * angle_resolution)
-    temp_pose = tf.transformations.quaternion_from_euler(0, beta, 0)
+  # for i in range(0, beta_range/angle_resolution + 1):
+  #   # pose = []
+  #   pose[1] = 0
+  #   pose[2] = 0
+  #   pose[3] = 0
+  #   beta = d_to_r * (- beta_range/2 + i * angle_resolution)
+  #   temp_pose = tf.transformations.quaternion_from_euler(0, beta, 0)
 
-    pose.orientation.x = temp_pose[0]
-    pose.orientation.y = temp_pose[1]
-    pose.orientation.z = temp_pose[2]
-    pose.orientation.w = temp_pose[3]
-    grasp[object_name][index] = pose
-    index += 1
+  #   pose[3] = temp_pose[0]
+  #   pose[4] = temp_pose[1]
+  #   pose[5] = temp_pose[2]
+  #   pose[6] = temp_pose[3]
+  #   grasp[index] = pose
+  #   index += 1
 
+  # grasp = [0, 0, 0, 0, 0, 0, 1]
 
   for j in range(1, alpha_range/angle_resolution + 1):
-    pose = Pose()
+    pose = [0]
+    pose.append(0)
+    pose.append(0)
+
     alpha = d_to_r * (- alpha_range/2 + j * angle_resolution)
-    print 'alpha = ', alpha
+    # print 'alpha = ', alpha
     temp_pose = tf.transformations.quaternion_from_euler(alpha, 0, 0)
 
-    pose.orientation.x = temp_pose[0]
-    pose.orientation.y = temp_pose[1]
-    pose.orientation.z = temp_pose[2]
-    pose.orientation.w = temp_pose[3]
-    grasp[object_name][index] = pose
-    print_angles(pose)
+    for i in range(0,4):
+      tmp =  float(temp_pose[i])
+      pose.append(tmp)
+
+    # pose.append(temp_pose[1])
+    # pose.append(temp_pose[2])
+    # pose.append(temp_pose[3])
+    grasp.append(pose)
+    # grasp = (grasp, pose)
+
+    # print_angles(pose)
     index += 1
 
+  print grasp
+  # for k in range(1, gamma_range/angle_resolution + 1):
+  #   gamma = d_to_r * (- gamma_range/2 + k * angle_resolution) 
+  #   temp_pose = tf.transformations.quaternion_from_euler(0, 0, gamma)
 
-  for k in range(1, gamma_range/angle_resolution + 1):
-    gamma = d_to_r * (- gamma_range/2 + k * angle_resolution) 
-    temp_pose = tf.transformations.quaternion_from_euler(0, 0, gamma)
-
-    pose.orientation.x = temp_pose[0]
-    pose.orientation.y = temp_pose[1]
-    pose.orientation.z = temp_pose[2]
-    pose.orientation.w = temp_pose[3]
-    grasp[object_name][index] = pose
-    print_angles(pose)
-    index += 1
+  #   pose.orientation.x = temp_pose[0]
+  #   pose.orientation.y = temp_pose[1]
+  #   pose.orientation.z = temp_pose[2]
+  #   pose.orientation.w = temp_pose[3]
+  #   grasp[index] = pose
+  #   # print_angles(pose)
+  #   index += 1
         # print index
         # print grasp
 
@@ -106,24 +117,32 @@ def grasp_main():
 #   with open(save_path) as f:
 #     loaded_grasp = json.load(f)
 
-  print index
-  save_path = './data/' + object_name
+  # print index
+  save_path = 'Github/grasp//data/' + object_name
   # print save_path
+  
   numpy.savez(save_path, grasp = [grasp])
   # grasp_publisher = rospy.Publisher('/grasp', , queue_size =10)
   # data_path = root_path + '/data/' + object_name + '.npy'
   # print data_path
-  loaded_file = numpy.load(save_path + '.npz' )
-  # print index
-  # print len(loaded_file[object_name])
+  a = [1, 2, 3, 4, 5, 6, 7]
+  a = [a, [1, 2, 3, 4, 5, 6, 7]]
+  # a = a.append([1, 2, 3, 4, 5, 6, 7])
+  # a = a.append([1, 2, 3, 4, 5, 6, 7])
+  with open(save_path + '.yaml', 'w') as yaml_file:
+      yaml.dump(grasp, yaml_file, default_flow_style=True)
 
-  loaded_grasp =  loaded_file['grasp'][0]
-  num_of_grasp = len(loaded_grasp[object_name])
-  print loaded_grasp
-  for i in range(0,num_of_grasp):
-    print i
-    print loaded_grasp[object_name][i]
-    print_angles(loaded_grasp[object_name][i])
+  # loaded_file = numpy.load(save_path + '.npz' )
+  # # print index
+  # # print len(loaded_file[object_name])
+
+  # loaded_grasp =  loaded_file['grasp'][0]
+  # num_of_grasp = len(loaded_grasp)
+  # print loaded_grasp
+  # for i in range(0,num_of_grasp):
+  #   print i
+  #   print loaded_grasp[object_name][i]
+  #   print_angles(loaded_grasp[object_name][i])
 
   # Main loop
   # rate = rospy.Rate(5) # hz
